4d0292443d4feba8d5594b9cc0025185dac81381
mau-io <maurice222@outlook.com>
2022-03-12 00:42:21 -0600

    Update cards functions

--- script.js
+++ script.js
@@ -1,50 +1,81 @@
-const rangeSliderValue = function (range) {
-    const displayRange = document.getElementById('priority');
-    const displayPriority = document.getElementById('priority-label');
+const generateId = () => {
+    const id = Math.random().toString(36).substring(2, 9);
+    return id;
+}
 
-    displayRange.innerHTML = range;
-    if (range == 1) {
-        //do stuff
-        displayRange.style.color = "blue";
-        displayPriority.style.color = "blue";
-        displayPriority.innerHTML = "Very Low";
-    } else if (range == 2) {
-        displayRange.style.color = "purple";
-        displayPriority.style.color = "purple";
-        displayPriority.innerHTML = "Low";
-    } else if (range == 3) {
-        displayRange.style.color = "yellow";
-        displayPriority.style.color = "yellow";
-        displayPriority.innerHTML = "Medium";
-
-    } else if (range == 4) {
-        displayRange.style.color = "orange";
-        displayPriority.style.color = "orange";
-        displayPriority.innerHTML = "High";
-    } else if (range == 5) {
-        displayRange.style.color = "red";
-        displayPriority.style.color = "red";
-        displayPriority.innerHTML = "Very High";
-    } else {
-        displayRange.style.color = "blue";
-        displayPriority.style.color = "blue";
-        displayPriority.innerHTML = "None";
+const defaultData = [
+    {
+        title: "title 1",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quaerat.",
+        id: 1
     }
-};
+]
 
-const start = () => {
-    // get "input[name=project]" element
-    let project = document.getElementsByName('project')[0]?.value;
+const elements = localStorage.getItem('data') 
+    ? JSON.parse(localStorage.getItem('data')) 
+    : defaultData;
+
+
+const updateLocalStorage = () => {
+    localStorage.setItem('data', JSON.stringify(elements));
+}
+
+const deleteElement = (id) => {
+    const index = elements.findIndex(element => element.id === id);
+    if (index !== -1) {
+        elements.splice(index, 1);
+    }
+}
+
+const createCard = ({title, details, id}) => {
+    const app = document.getElementsByClassName('titles')[0];
+    const card = document.createElement('div');
+
+    // set id
+    card.setAttribute('id', id);
+
+    card.innerHTML = `
+        <div class="title">
+            <h2> ${title} </h2>
+            <span class="close">X</span> 
+            <p> ${details} </p>
+        </div>
+    `;
+    // add event listener to the close button
+    card
+        .getElementsByClassName('close')[0]
+        .addEventListener('click', () => {
+            
+            // remove the card from the elements by id atributes
+            deleteElement(id);
+
+            // update localStorage
+            updateLocalStorage();
+
+            // remove the card from the DOM
+            card.remove();
+
+            console.log(elements);
+    });
+
+    app.appendChild(card);
+}
+
+
+
+const addCard = () => {
+    // get "input[name=title]" element
+    let title = document.getElementsByName('title')[0]?.value;
     // get "input[name=details]" element
     let details = document.getElementsByName('details')[0]?.value;
 
     //if has title
-    if (project.length > 0) {
-        project = project
+    if (title.length > 0) {
+        title = title
     }
     // doesnt have title    
     else {
-        project = "N/A"
+        title = "N/A"
     }
 
     if (details.length > 0) {
@@ -53,30 +84,30 @@ const start = () => {
         details = "N/A"
     }
 
-    const app = document.getElementsByClassName('projects')[0];
-    const card = document.createElement('div');
 
-    card.innerHTML = `
-        <div class="project">
-            <h2> ${project} </h2>
-            <span class="close">X</span> 
-            <p> ${details} </p> 
-            <h3>Priority</h3>
-            <div id="priority-wrap">
-                <span id="priority" class="priority">1</span> - <span class="priority-label" id="priority-label"> Very Low </span> 
-            </div>
-            <input class="priority-slider" type="range" value="1" min="1" max="5" step="1" onChange="rangeSliderValue(this.value)">
-        </div>
-    `;
-    // add event listener to the close button
-    card.getElementsByClassName('close')[0].addEventListener('click', function () {
-        card.remove();
-    });
-    app.appendChild(card);
+    const card = {
+        title,
+        details,
+        id: generateId()
+    }
+
+    // add card to the elements array
+    elements.push(card);
+
+    // update localStorage
+    updateLocalStorage();
+    
+    // create the card in the DOM
+    createCard(card);
 }
 
 const button = document.getElementsByClassName('submit')[0];
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    start();
+    addCard();
 })
+
+for(element of elements) {
+    createCard(element);
+}
